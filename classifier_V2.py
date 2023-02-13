7216aa46ef91e39e19275b5ca2fb3fa154c71f36
tristangclvs <goncalvestristan.pro@pm.me>
2023-02-13 14:38:38 +0100

    Initial Commit

---
+++ classifier_V2.py
@@ -0,0 +1,99 @@
+# Imports
+import json
+import numpy as np  # linear algebra
+from sklearn.model_selection import train_test_split
+import tensorflow.keras as keras
+import matplotlib.pyplot as plt
+
+
+# =============================================================================
+
+def load_data(dataset_path):
+    with open(dataset_path, "r") as data_file:
+        data = json.load(data_file)
+    # convert lists into numpy arrays
+    inputs = np.array(data["mfcc"])
+    targets = np.array(data["labels"])
+    return inputs, targets
+
+
+def plot_history(history):
+    fig, axs = plt.subplots(2)
+
+    # create accuracy subplot
+    axs[0].plot(history.history["accuracy"], label="train accuracy")
+    axs[0].plot(history.history["val_accuracy"], label="test accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].legend(loc="lower right")
+    axs[0].set_title("Accuracy eval")
+
+    # create accuracy subplot
+    axs[1].plot(history.history["loss"], label="train loss")
+    axs[1].plot(history.history["val_loss"], label="test loss")
+    axs[1].set_ylabel("Loss")
+    axs[1].set_xlabel("Epoch")
+    axs[1].legend(loc="lower right")
+    axs[1].set_title("Loss eval")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    # ============================================================
+    # Same as V1 but with the implementation of Dropout technique||
+    # ============================================================
+
+    # load data
+    inputs, targets = load_data("data.json")
+    print(inputs.shape)
+    print(targets.shape)
+
+    # load data
+
+    # split the data into train and test sets
+    inputs_train, inputs_test, targets_train, targets_test = train_test_split(inputs, targets, test_size=0.3)
+
+    # build the network architecture
+    model = keras.Sequential([
+
+        keras.layers.Flatten(input_shape=(inputs.shape[1], inputs.shape[2])),
+
+        # 1st hidden layer
+        keras.layers.Dense(512, activation="relu"),
+        # , kernel_regularizer=keras.regularizers.l2(0.001)
+        keras.layers.Dropout(0.2),
+
+        # 2nd hidden layer
+        keras.layers.Dense(256, activation="relu"),  # , kernel_regularizer=keras.regularizers.l2(0.001)
+        keras.layers.Dropout(0.2),
+
+        # 3rd hidden layer
+        keras.layers.Dense(64, activation="relu"),  # , kernel_regularizer=keras.regularizers.l2(0.001)
+        keras.layers.Dropout(0.2),
+
+        # output layer
+        # 10 neurons because we have 10 genres
+        keras.layers.Dense(10, activation="softmax")
+    ])
+
+    # compile network
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001)
+
+    model.compile(optimizer=optimizer,
+                  loss="sparse_categorical_crossentropy",
+                  metrics=["accuracy"])
+
+    print(model.summary())
+
+    # train network
+    # mini-batch between 16 and 128 samples
+    # batch_size is the number of samples that will be propagated through the network
+    history = model.fit(inputs_train, targets_train,
+                        validation_data=(inputs_test, targets_test),
+                        epochs=50, batch_size=32)
+
+    # plot accuracy and error over the epochs
+    plot_history(history)
+
+    _, accuracy = model.evaluate(inputs_test, targets_test, batch_size=32)
+    print(accuracy)
