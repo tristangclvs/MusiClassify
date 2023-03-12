6efc765511c3c36934a9f8df8bfadb3b58748e5c
tristangclvs <goncalvestristan.pro@pm.me>
2023-03-12 20:09:14 +0100

    audio trimmer in a function

--- music_cutter.py
+++ music_cutter.py
@@ -5,30 +5,33 @@ import os
 from audio_converters import audio_to_wav
 import simpleaudio as sa
 
-genre: str = input("Enter the genre of the musics you want to scrape: ")
 
-file_path = f"downloads\\{genre.capitalize()}\\"
-
-sr = 22050
-for file in os.listdir(file_path):
-    abs_file_path = os.path.join(os.getcwd(), file_path, file)
-
-    # print(abs_file_path)
+def trim_audio(abs_file_path, start_time, end_time, write=False, sr=22050):
     audio_path = audio_to_wav(abs_file_path)
-    # print(audio)
-
     signal, sample_rate = librosa.load(audio_path, sr=sr)
-
-    # Set the start and end times for trimming (in seconds)
-    start_time = 0
-    end_time = 30
-
     # Calculate the start and end samples for trimming
-    start_sample = int(start_time * sr)
-    end_sample = int(end_time * sr)
+    start_sample: int = int(start_time * sr)
+    end_sample: int = int(end_time * sr)
 
     # Trim the audio file
     trimmed_audio = signal[start_sample:end_sample]
 
-    # Export the trimmed audio to a file
-    sf.write(f"{file.split('.')[0]}.wav", trimmed_audio, sr)
+    if write:
+        # Export the trimmed audio to a file
+        sf.write(f"{file.split('.')[0]}.wav", trimmed_audio, sr)
+
+    return trimmed_audio
+
+
+# ======================================================================================
+genre: str = input("Enter the genre of the musics you want to scrape: ")
+
+file_path: str = f"downloads\\{genre.capitalize()}\\"
+
+for file in os.listdir(file_path):
+    abs_file_path: str = os.path.join(os.getcwd(), file_path, file)
+    for i in range(3):
+        # Set the start and end times for trimming (in seconds)
+        start_time = 30 * i
+        end_time = 30 * i + 30
+        trimmed_audio = trim_audio(abs_file_path, start_time, end_time)
